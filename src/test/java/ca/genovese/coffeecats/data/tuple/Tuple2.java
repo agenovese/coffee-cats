f4c0bc73a80f74349f0a846a28d96d26c430637f
Angelo Genovese <angelo@streetcontxt.com>
2016-05-28 19:14:11 -0400

    Add a tuple2 data structure

---
+++ src/test/java/ca/genovese/coffeecats/data/tuple/Tuple2.java
@@ -0,0 +1,17 @@
+package ca.genovese.coffeecats.data.tuple;
+
+import java.util.function.BiFunction;
+
+public class Tuple2<A, B> {
+  public final A _1;
+  public final B _2;
+
+  public Tuple2(A a, B b) {
+    _1 = a;
+    _2 = b;
+  }
+
+  public <R> R applyTo(BiFunction<A,B,R> f) {
+    return f.apply(_1, _2);
+  }
+}
