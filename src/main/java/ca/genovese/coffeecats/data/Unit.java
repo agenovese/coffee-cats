fc1caf1aea74ff71d7fca1f9e0026b36fa48c75e
Angelo Genovese <angelo@streetcontxt.com>
2016-06-04 13:17:08 -0400

    Add JavaDoc for Unit

--- src/main/java/ca/genovese/coffeecats/data/Unit.java
+++ src/main/java/ca/genovese/coffeecats/data/Unit.java
@@ -1,8 +1,17 @@
 package ca.genovese.coffeecats.data;
 
+/**
+ * A "void" type
+ */
 public final class Unit {
+  /**
+   * The single instance of unit, allowing for == comparisons
+   */
   public static final Unit unit = new Unit();
 
+  /**
+   * construct a unit value
+   */
   private Unit() {
   }
 }
