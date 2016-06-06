77c9678b3f442d260039ca48ed5564b1192aeec1
Angelo Genovese <angelo@streetcontxt.com>
2016-06-06 09:40:33 -0400

    Add JavaDoc to the kind package

--- src/main/java/ca/genovese/coffeecats/kind/Kind.java
+++ src/main/java/ca/genovese/coffeecats/kind/Kind.java
@@ -1,6 +1,17 @@
 package ca.genovese.coffeecats.kind;
 
+/**
+ * Interface representing a type with one type argument.
+ *
+ * @param <F> The type
+ * @param <A> The first type argument
+ */
 public interface Kind<F, A> {
+  /**
+   * A convenience method for getting out a value casted to the non-generic type.
+   *
+   * @return the value of this "Kind" as a non-generic type.
+   */
   @SuppressWarnings("unchecked")
   default F getRealType() {
     return (F) this;
---
+++ src/main/java/ca/genovese/coffeecats/kind/package-info.java
@@ -0,0 +1,29 @@
+/**
+ * This package contains Interfaces allowing the partial
+ * representation of parametric polymorphic types in Java.
+ *
+ * <p>For any KindN the first type argument is the paramaterized
+ * type, the following N arguments are the type's arguments.
+ *
+ * <p>For example, this allows us to represent Functor as:
+ * <pre>
+ * <code>
+ * interface Functor<F> {
+ *   <A, B> Kind<F, B> map(Kind<F, A> fa, Function<A, B> f);
+ * }
+ * </code>
+ * </pre>
+ *
+ * <p>Which would otherwise be represented as something like the
+ * following in a Java like language that allowed parametric
+ * polymorphism
+ * <pre>
+ * <code>
+ * interface Functor<F> {
+ *   <A, B> F<B> map(F<A> fa, Function<A, B> f);
+ * }
+ * </code>
+ * </pre>
+ */
+package ca.genovese.coffeecats.kind;
+
